5278cb05ca88ca7712ce47583a9dd2cb7a84969f
Glauber Vilane Ferreira Marques <gvfmarques@hotmail.com>
2022-03-12 21:24:48 -0600

    Feat:Add dependency Injections functionalities and Sql Connection.

---
+++ RestApiModelDDDInfrastructure/CrossCutting/IOC/ConfigurationIOC.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using RestApiModelDDDApplication;
+using RestApiModelDDDApplication.Interfaces;
+using RestApiModelDDDApplication.Interfaces.Mappers;
+using RestApiModelDDDApplication.Mappers;
+using RestApiModelDDDInfrastructure.Data.Repositories;
+using RestApiModelDDDomain.Core.Interfaces.Repositories;
+using RestApiModelDDDomain.Core.Interfaces.Services;
+using RestApiModelDDDomain.Services;
+
+namespace RestApiModelDDDInfrastructure.CrossCutting.IOC
+{
+    public class ConfigurationIOC
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IOC
+
+            builder.RegisterType<ApplicationServiceClient>().As<IApplicationServiceClient>();
+            builder.RegisterType<ApplicationServiceProduct>().As<IApplicationServiceProduct>();
+            builder.RegisterType<ServiceClient>().As<IServiceClient>();
+            builder.RegisterType<ServiceProduct>().As<IServiceProduct>();
+            builder.RegisterType<RepositoryClient>().As<IRepositoryClient>();
+            builder.RegisterType<RepositoryProduct>().As<IRepositoryProduct>();
+            builder.RegisterType<MapperClient>().As<IMapperClient>();
+            builder.RegisterType<MapperProduct>().As<IMapperProduct>();
+
+            #endregion
+
+        }
+    }
+}
