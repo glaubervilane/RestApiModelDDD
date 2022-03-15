01b46cf87a08392592c3ab9756a6a5fddab5dd37
Glauber Vilane Ferreira Marques <gvfmarques@hotmail.com>
2022-03-14 20:14:53 -0500

    Feat:Launch Clients and Products Controllers with its functionalities

---
+++ RestApiModelDDDAPI/Controllers/ClientsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiModelDDDApplication.Dtos;
+using RestApiModelDDDApplication.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiModeloDDD.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+
+        private readonly IApplicationServiceClient applicationServiceClient;
+
+
+        public ClientsController(IApplicationServiceClient applicationServiceClient)
+        {
+            this.applicationServiceClient = applicationServiceClient;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(applicationServiceClient.GetAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(applicationServiceClient.GetById(id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] ClientDto clientDTO)
+        {
+            try
+            {
+                if (clientDTO == null)
+                    return NotFound();
+
+                applicationServiceClient.Add(clientDTO);
+                return Ok("Client Registered Successfuly!");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public ActionResult Put([FromBody] ClientDto clientDTO)
+        {
+            try
+            {
+                if (clientDTO == null)
+                    return NotFound();
+
+                applicationServiceClient.Update(clientDTO);
+                return Ok("Client Updated Successfuly!");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete()]
+        public ActionResult Delete([FromBody] ClientDto clientDTO)
+        {
+            try
+            {
+                if (clientDTO == null)
+                    return NotFound();
+
+                applicationServiceClient.Remove(clientDTO);
+                return Ok("Client Delteted Successfuly!");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ RestApiModelDDDAPI/Controllers/ProductsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiModelDDDApplication.Dtos;
+using RestApiModelDDDApplication.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiModeloDDD.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IApplicationServiceProduct applicationServiceProduct;
+
+        public ProductsController(IApplicationServiceProduct applicationServiceProduct)
+        {
+            this.applicationServiceProduct = applicationServiceProduct;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(applicationServiceProduct.GetAll());
+        }
+
+        // GET api/values/5\
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(applicationServiceProduct.GetById(id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] ProductDto productDTO)
+        {
+            try
+            {
+                if (productDTO == null)
+                    return NotFound();
+
+
+                applicationServiceProduct.Add(productDTO);
+                return Ok("Product Registered Successfuly!");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public ActionResult Put([FromBody] ProductDto productDTO)
+        {
+
+            try
+            {
+                if (productDTO == null)
+                    return NotFound();
+
+                applicationServiceProduct.Update(productDTO);
+                return Ok("Product Updated Successfuly!");
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete()]
+        public ActionResult Delete([FromBody] ProductDto productDTO)
+        {
+            try
+            {
+                if (productDTO == null)
+                    return NotFound();
+
+                applicationServiceProduct.Remove(productDTO);
+                return Ok("Product Delteted Successfuly!");
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
